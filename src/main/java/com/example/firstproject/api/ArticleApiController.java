ee30374928267c9deb558b095189b4ee5e411ef2
morendo <morendo90@gmail.com>
2024-02-23 07:45:26 +0900

    #12 - 서비스 계층 만들기
    컨트롤러의 비즈니스 로직 서비스에 위임

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,6 +3,7 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepostiory;
+import com.example.firstproject.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,56 +17,44 @@ import java.util.List;
 public class ArticleApiController {
     @Autowired
     private ArticleRepostiory articleRepostiory;
+    @Autowired
+    private ArticleService articleService; //서비스 객체 주입
     // GET
     @GetMapping("/api/articles")
     public List<Article> index() {
-        return articleRepostiory.findAll();
+        return articleService.index();
     }
 
     @GetMapping("/api/articles/{id}")
     public Article show(@PathVariable Long id) {
-        return articleRepostiory.findById(id).orElse(null);
+        return articleService.show(id);
     }
 
     // POST
     @PostMapping("/api/articles")
-    public Article create(@RequestBody ArticleForm dto) {
-        Article article = dto.toEntity();
-        return articleRepostiory.save(article);
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto) {
+        Article created = articleService.create(dto);
+        return (created != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
-
 
     // PATCH
     @PatchMapping("/api/articles/{id}")
     public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
-        // 1. dto => 엔티티 변환하기
-        Article article = dto.toEntity();
-        log.info("id: {}, article: {}", id, article.toString());
-        // 2. 타깃 조회하기
-        Article target = articleRepostiory.findById(id).orElse(null);
-        // 3. 잘못된 요청 처리하기
-        if(target == null || id != article.getId()) {
-            //400 잘못된 요청 응답
-            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 4. 업데이트 및 정상 응답(200) 하기
-        target.patch(article);
-        Article updated = articleRepostiory.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
+        Article updated = articleService.update(id, dto);
+        return(updated != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(updated):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     // DELETE
     @DeleteMapping("/api/articles/{id}")
     public ResponseEntity<Article> delete(@PathVariable Long id) {
-        // 1. 대상찾기
-        Article target = articleRepostiory.findById(id).orElse(null);
-        // 2. 잘못된 요청 처리하기
-        if(target == null)
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        // 3. 대상 삭제하기
-        articleRepostiory.delete(target);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        Article deleted = articleService.delete(id);
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(deleted):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 
     }
 }
